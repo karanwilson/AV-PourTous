69462b9c2057681e87dd2a361a9ae56b5337e2a2
Karan Wilson <karan.wilson@auroville.org.in>
2024-09-24 13:25:00 +0530

    CI: Added Purchase Receipt client script edit PO print format

---
+++ purchase_receipt_script.js
@@ -0,0 +1,27 @@
+frappe.ui.form.on('Purchase Receipt', {
+	refresh(frm) {
+        frm.set_value('taxes_and_charges', 'Input GST In-state - PTPS')
+            .then(() => {
+                frm.refresh_field('taxes_and_charges');
+            })
+	},
+
+    supplier(frm) {
+        frm.set_query('item_code', 'items', () => {
+            console.log('set_query trigerred');
+            return {
+                query: 'pourtous.api.supplier_items_filter',
+                txt: frm.doc.supplier
+            };
+        });
+    },
+
+    after_save(frm) {
+        let custom_rate_with_tax = 0;
+        frm.doc.items.forEach((item) => {
+            custom_rate_with_tax = (item.amount + item.cgst_amount + item.sgst_amount)/item.qty;
+            item.custom_rate_with_tax = custom_rate_with_tax;
+            frm.refresh_field(frm.doc.items);
+        });
+    }
+});
