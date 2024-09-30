adb5437535c60346dea10790fdbdd4cfba7302e6
Karan Wilson <karan.wilson@auroville.org.in>
2024-09-30 11:43:00 +0530

    CI

--- sync_fs_accounts.js
+++ sync_fs_accounts.js
@@ -1,7 +1,15 @@
 frappe.listview_settings['Customer'] = {
     refresh(listview) {
         listview.page.add_inner_button("Sync FS Accounts", () => {
-            frappe.call('pourtous.api.sync_fs_accounts');
+            frappe.call({
+                method: 'pourtous.api.sync_fs_accounts',
+                freeze: true,
+                freeze_message: "Syncing FS Accounts",
+                callback: (r) => {
+                    //this.refresh();
+                    location.reload();
+                }
+            });
         });
     },
 };
