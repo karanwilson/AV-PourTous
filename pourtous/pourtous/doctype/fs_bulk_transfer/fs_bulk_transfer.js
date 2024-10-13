0ceed744cc026470f10eb5d90863d750b98e1f46
Karan Wilson <karan.wilson@auroville.org.in>
2024-10-13 15:49:13 +0530

    testing: FS Bulk transfer logs

--- pourtous/pourtous/doctype/fs_bulk_transfer/fs_bulk_transfer.js
+++ pourtous/pourtous/doctype/fs_bulk_transfer/fs_bulk_transfer.js
@@ -4,38 +4,40 @@
 frappe.ui.form.on('FS Bulk Transfer', {
 	// refresh: function(frm) {
 	// }
-	initiate(frm) {
-		if (frm.doc.export_transfers == "Pending") {
-            frappe.call({
-                method: 'payments.payment_gateways.doctype.fs_settings.fs_settings.add_transfer_fs_draft_bills',
-				args: {
-					'name': frm.doc.name,
-					'from_date': frm.doc.from_date,
-					'to_date': frm.doc.to_date
-				},
-                freeze: true,
-                freeze_message: "Processing Offline FS Bills",
-                callback: (r) => {
-                    frm.refresh_field(frm.doc.transaction_logs)
-                    //location.reload();
-                }
-            });
-		}
-		else if (frm.doc.export_transfers == "Insufficient Funds") {
-            frappe.call({
-                method: 'payments.payment_gateways.doctype.fs_settings.fs_settings.add_transfer_fs_credit_bills',
-				args: {
-					'name': frm.doc.name,
-					'from_date': frm.doc.from_date,
-					'to_date': frm.doc.to_date
-				},
-                freeze: true,
-                freeze_message: "Processing FS Credit Bills",
-                callback: (r) => {
-                    frm.refresh_field(frm.doc.transaction_logs)
-                    //location.reload();
-                }
-            });
+	after_save(frm) {
+        console.log("frm.doc: ", frm.doc);
+        switch(frm.doc.export_transfers) {
+            case "Pending":
+                frappe.call({
+                    method: 'payments.payment_gateways.doctype.fs_settings.fs_settings.add_transfer_fs_draft_bills',
+		    		args: {
+			    		'doc_name': frm.doc.name,
+				    	'year': frm.doc.year,
+    				},
+                    freeze: true,
+                    freeze_message: "Processing Offline FS Bills",
+                    callback: (r) => {
+                        frm.refresh_field(frm.doc.transaction_logs)
+                        //location.reload();
+                    }
+                });
+                break;
+
+            case "Insufficient Funds":
+                frappe.call({
+                    method: 'payments.payment_gateways.doctype.fs_settings.fs_settings.add_transfer_fs_credit_bills',
+                    args: {
+                        'doc_name': frm.doc.name,
+                        'year': frm.doc.year,
+                    },
+                    freeze: true,
+                    freeze_message: "Processing FS Credit Bills",
+                    callback: (r) => {
+                        frm.refresh_field(frm.doc.transaction_logs)
+                        //location.reload();
+                    }
+                });
+                break;
 		}
 	}
 });
--- pourtous/pourtous/doctype/fs_bulk_transfer_logs/fs_bulk_transfer_logs.py
+++ pourtous/pourtous/doctype/fs_bulk_transfer_logs/fs_bulk_transfer_logs.py
@@ -4,5 +4,5 @@
 # import frappe
 from frappe.model.document import Document
 
-class ProcessFSBillLogs(Document):
+class FSBulkTransferLogs(Document):
 	pass
