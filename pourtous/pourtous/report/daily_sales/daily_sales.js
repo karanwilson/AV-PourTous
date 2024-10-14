333ea4816398ef6a6916dfef6e9d1d2d8d24e6aa
Karan Wilson <karan.wilson@auroville.org.in>
2024-10-14 21:40:05 +0530

    CI: daily sales report work-in-progress

---
+++ pourtous/pourtous/report/daily_sales/daily_sales.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, Karan and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Daily Sales"] = {
+	"filters": [
+
+	]
+};
