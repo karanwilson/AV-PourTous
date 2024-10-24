c439491bc51ecd0e4897f6cd038787af5b7a6386
Karan Wilson <karan.wilson@auroville.org.in>
2024-10-24 09:57:26 +0530

    CI: Sales report WIP

--- pourtous/pourtous/report/daily_sales/daily_sales.py
+++ pourtous/pourtous/report/daily_sales/daily_sales.py
@@ -1,7 +1,9 @@
 # Copyright (c) 2024, Karan and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe, erpnext
+from frappe import _, msgprint
+from frappe.utils import getdate
 
 
 def execute(filters=None):
@@ -9,6 +11,21 @@ def execute(filters=None):
 		return [], []
 	# validate_filters(filters)
 
-
 	columns, data = [], []
+
+	columns = get_columns()
+	data = get_data(filters)
+
+	if not data:
+		msgprint(_('No records found'))
+		return columns, data
+
 	return columns, data
+
+
+def get_columns():
+	pass
+
+
+def get_data(filters):
+	current_date = getdate()
