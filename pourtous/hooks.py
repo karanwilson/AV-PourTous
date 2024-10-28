e39bf082951dc744a4fa1ae5288c8ab748326ef1
Karan Wilson <karan.wilson@auroville.org.in>
2024-10-28 21:12:15 +0530

    CI: Pur Ord query WIP

--- pourtous/hooks.py
+++ pourtous/hooks.py
@@ -132,7 +132,8 @@ doc_events = {
     # comment the hook below until the pricing rule/method is defined
 	"Purchase Receipt": {
 		"on_submit": "pourtous.api.update_selling_price_list", # creates an 'Item Price' in the 'Selling Price List'
-		"before_cancel": "pourtous.api.delete_item_price" # deletes the linked 'Item Price' before cancelling the Purchase Receipt
+		"before_cancel": "pourtous.api.delete_item_price", # deletes the linked 'Item Price' before cancelling the Purchase Receipt
+        "after_delete": "pourtous.api.delete_item_price" # deletes the linked 'Item Price' before cancelling the Purchase Receipt
 	},
 # 	"*": {
 # 		"on_update": "method",
@@ -251,9 +252,14 @@ fixtures = [
                     "Customer-custom_fs_account_number", # for FS Transactions
                     "Customer-custom_fs_kind_account_3", # for FS Transactions
                     "Customer-custom_fs_cash_account_4", # for FS Transactions
-                    #"Purchase Order-custom_supplier_batch_items_fetch", # Creates a Tab Break for custom_supplier_items_data and custom_add_to_items
-                    "Purchase Order-custom_supplier_items_data", # To fetch the Items from the Selected Supplier, along with the required data
-                    "Purchase Order-custom_add_to_items", # pushes the Selected Items (and their Qty) to the PO Items
+                    "Purchase Order-custom_batch_items_fetch", # Creates a Tab Break for custom_batch_items_data and custom_add_batch_items
+                    "Purchase Order-custom_batch_items_data", # To fetch the Items from the Selected Supplier, along with the required data
+                    "Purchase Order-custom_add_batch_items", # pushes the Selected Items (and their Qty) to the PO Items
+
+                    """ "Purchase Order-custom_batch_items_fetch", # Creates a Tab Break for custom_batch_items_data and custom_add_batch_items
+                    "Purchase Order-custom_batch_items_data", # To fetch the Items from the Selected Supplier, along with the required data
+                    "Purchase Order-custom_add_batch_items", # pushes the Selected Items (and their Qty) to the PO Items """
+
                     "Purchase Receipt Item-custom_rate_with_tax"
 				)
 			]
