50d2ddf0b7ac7a4a7ee632c20b9bc1a3e9dd17fb
Karan Wilson <karan.wilson@auroville.org.in>
2024-11-28 23:43:57 +0530

    fix: Modified script for "Process FS Credit Bills"

--- process_fs_bills.js
+++ process_fs_bills.js
@@ -14,17 +14,34 @@ frappe.listview_settings['Sales Invoice'] = {
 
         listview.page.add_inner_button("Process FS Credit Bills", () => {
             frappe.call({
-                method: 'payments.payment_gateways.doctype.fs_settings.fs_settings.add_transfer_fs_credit_bills',
-                freeze: true,
-                freeze_message: "Processing FS Credit Bills",
+                method: 'payments.payment_gateways.doctype.fs_settings.fs_settings.fetch_fs_credit_bills',
                 callback: (r) => {
-                    //this.refresh();
-                    location.reload();
+                    if (r.message) {
+                        const length = r.message.length;
+                        console.log("Number of Credit Bills to process: ", length);
+                        let transfers = 0;
+                        for (let i = 0; i < length; i++) {
+                            setTimeout(() => {
+                                frappe.call({
+                                    method: 'payments.payment_gateways.doctype.fs_settings.fs_settings.add_transfer_fs_credit_bill',
+                                    args: { bill: r.message[i][0] },
+                                    async: false,
+                                    callback: (r) => {
+                                        if (r.message == "OK")
+                                            transfers++;
+                                    }
+                                });
+                                const count = i+1;
+                                const message = "Loading "+count+" of "+length;
+                                frappe.show_progress("Processing FS Credit Bills", count, length, message);
+                            }, 0);
+                        }
+                    }
                 }
             });
         });
 
-        listview.page.add_inner_button("Exception Process FS Credit Bills", () => {
+        /* listview.page.add_inner_button("Exception Process FS Credit Bills", () => {
             frappe.call({
                 method: 'payments.payment_gateways.doctype.fs_settings.fs_settings.exception_add_transfer_fs_credit_bills',
                 freeze: true,
@@ -34,6 +51,6 @@ frappe.listview_settings['Sales Invoice'] = {
                     location.reload();
                 }
             });
-        });
+        }); */
     },
 };
