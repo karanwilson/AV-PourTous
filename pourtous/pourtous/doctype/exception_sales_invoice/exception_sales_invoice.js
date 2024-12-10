74a64777933d79e8f5dea0ad7a9d2ac650520ca5
Karan Wilson <karan.wilson@auroville.community>
2024-12-10 22:15:25 +0530

    CI: removing non-relevant doctype for Maroma.

--- pourtous/pourtous/doctype/exception_sales_invoice/exception_sales_invoice.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Karan and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Exception Sales Invoice', {
-	// refresh: function(frm) {
-
-	// }
-});
--- pourtous/pourtous/doctype/exception_sales_invoice/test_exception_sales_invoice.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Karan and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestExceptionSalesInvoice(FrappeTestCase):
-	pass
