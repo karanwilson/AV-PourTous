a4349af25d845bfceeac4eb7c3e715e85907c742
Karan Wilson <karan.wilson@auroville.org.in>
2025-01-05 18:20:59 +0530

    feat: Added "Negative Stock Report" and "Zero Stock Report"

---
+++ pourtous/pourtous/report/zero_stock_report/zero_stock_report.py
@@ -0,0 +1,98 @@
+# Copyright (c) 2025, Karan and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _, msgprint
+
+
+def execute(filters=None):
+	columns, data = [], []
+
+	columns = get_columns()
+	data = get_data()
+
+	if not data:
+		msgprint(_('No records found'))
+		return columns, data
+
+	return columns, data
+
+
+def get_columns():
+	return [
+		{
+			"fieldname": "item_code",
+			"label": "Item Code",
+			"fieldtype": "Data",
+			"width": "90"
+		},
+		{
+			"fieldname": "item_name",
+			"label": "Item Name",
+			"fieldtype": "Data",
+			"width": "350"
+		},
+		{
+			"fieldname": "supplier",
+			"label": "Supplier",
+			"fieldtype": "Data",
+			"width": "300"
+		},
+		{
+			"fieldname": "store_qty",
+			"label": "Store Qty",
+			"fieldtype": "Float",
+			"width": "100"
+		},
+		{
+			"fieldname": "stall_qty",
+			"label": "Stall Qty",
+			"fieldtype": "Float",
+			"width": "100"
+		},
+	]
+
+
+def get_data():
+	query = frappe.db.sql(
+		"""
+		SELECT tabItem.item_code, tabItem.item_name, `tabItem Supplier`.supplier,
+		(
+			select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+			where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+			and warehouse like '{0}'
+			order by posting_date desc, posting_time desc, creation desc
+			limit 1
+		) AS store_qty,
+		(
+			select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+			where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+			and warehouse like '{1}'
+			order by posting_date desc, posting_time desc, creation desc
+			limit 1
+		) AS stall_qty
+		FROM tabItem, `tabItem Supplier`
+		WHERE tabItem.item_code = `tabItem Supplier`.parent
+		AND
+		(
+		(
+			select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+			where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+			and warehouse like '{0}'
+			order by posting_date desc, posting_time desc, creation desc
+			limit 1
+		) = 0
+		OR
+		(
+			select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+			where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+			and warehouse like '{1}'
+			order by posting_date desc, posting_time desc, creation desc
+			limit 1
+		) = 0
+		)
+		""".format("Stores%", "Stall%"),
+		as_dict=True
+	)
+
+	return query
