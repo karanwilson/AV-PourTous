9b4e5643244c7e27686323c77776df797bcbc7a4
Karan Wilson <karan.wilson@auroville.org.in>
2025-01-13 17:51:43 +0530

    feat: Processing FS payments against Sales Orders

---
+++ process_SO_FS_payments.js
@@ -0,0 +1,37 @@
+frappe.listview_settings['Sales Order'] = {
+    refresh(listview) {
+        listview.page.add_inner_button("Process SO FS-Payments", () => {
+            frappe.call({
+                method: 'payments.payment_gateways.doctype.fs_settings.fs_settings.fetch_unpaid_sales_orders',
+                async: false,
+                callback: (r) => {
+                    if (r.message) {
+                        const length = r.message.length;
+                        console.log("Number of SO Payments to process: ", length);
+                        let transfers = 0;
+                        for (let i = 0; i < length; i++) {
+                            setTimeout(() => {
+                                frappe.call({
+                                    method: 'payments.payment_gateways.doctype.fs_settings.fs_settings.add_transfer_sales_order',
+                                    args: { order: r.message[i][0] },
+                                    async: false,
+                                }).then(r => {
+                                    if (r.message == "OK")
+                                        transfers++;
+                                }).then(r => {
+                                    // placing this statement block here as it does not work outside of the main frappe.call block
+                                    // though it prints on console for each loop iteration (comes in only one line, with the loop count),
+                                    // it shows an accurate result in the end. This design works.
+                                    console.log("Received transfers for ", transfers, " of ", length, " Sales Orders");
+                                });
+                                const count = i+1;
+                                const message = "Loading "+count+" of "+length;
+                                frappe.show_progress("Processing Sales Order Payments", count, length, message);
+                            }, 0);
+                        }
+                    }
+                }
+            });
+        });
+    },
+};
