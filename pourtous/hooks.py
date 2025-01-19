71f22dd1266fb1cf22972ef6f48e44c12827eece
Karan Wilson <karan.wilson@auroville.org.in>
2025-01-19 21:41:05 +0530

    CI: added comments field in PO Item, for mentioning custom UOMs, like bag, box, etc.

--- pourtous/hooks.py
+++ pourtous/hooks.py
@@ -271,6 +271,8 @@ fixtures = [
                     "Purchase Order-custom_non_batch_items_data", # To fetch the Items from the Selected Supplier, along with the required data
                     "Purchase Order-custom_add_non_batch_items", # Button: pushes the Selected Items (and their Qty) to the PO Items
 
+                    "Purchase Order Item-custom_comments", # for putting custom UOM (like bag, etc.) in Purchase Orders
+
                     "Stock Reconciliation Item-custom_comments", # to add comments regarding a stock reconciliation
                     #"Purchase Receipt Item-custom_rate_with_tax"
 				)
