0da782ff8b39f4ad0204319c1462b433d5698be7
Karan Wilson <karan.wilson@auroville.org.in>
2025-02-11 19:00:03 +0530

    CI: deleted unused Doctypes

--- pourtous/pourtous/doctype/fs_bulk_transfer_logs/fs_bulk_transfer_logs.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024, Karan and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class FSBulkTransferLogs(Document):
-	pass
