7f77a89441895254cf22c46a6b8f302f7a7b7bd6
Karan Wilson <karan.wilson@auroville.org.in>
2025-02-13 18:50:19 +0530

    feat: zoho_books_api WIP

---
+++ pourtous/pourtous/doctype/zoho_books_api/zoho_books_api.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Karan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Zoho Books API', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ pourtous/pourtous/doctype/zoho_books_api/zoho_books_api.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2025, Karan and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class ZohoBooksAPI(Document):
+	pass
+
+
+def fetch_sales_invoice_list():
+	pass
+
+
+def sync_with_zoho_books():
+	pass
