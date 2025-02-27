aacc313ba0ee2f2894a1c34aeb3ca70b2bafca07
Karan Wilson <karan.wilson@auroville.org.in>
2025-02-27 11:14:38 +0530

    CI: Zoho Books WIP

--- pourtous/pourtous/doctype/zoho_books_api/zoho_books_api.py
+++ pourtous/pourtous/doctype/zoho_books_api/zoho_books_api.py
@@ -4,32 +4,17 @@
 import frappe
 from frappe.model.document import Document
 
-import requests
-#import urllib
-#from requests_oauthlib import OAuth2Session
-#from oauthlib.oauth2 import BackendApplicationClient
+#import requests
+import urllib
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import BackendApplicationClient
 
 class ZohoBooksAPI(Document):
 	@frappe.whitelist(allow_guest=True)
 	def zoho_api_token(self, throw_if_missing=False):
-		with requests.Session() as s:
-			s.params = {
-				'client_id': self.client_id,
-				'client_secret': self.client_secret,
-				'grant_type': self.grant_type,
-				'scope': self.scope,
-				'soid': self.soid
-				}
-
-			r = s.get(self.token_url)
-			r.raise_for_status()
-
-			return r.json()
-
-
-		""" client_id = ''
-		client_secret = ''
-		grant_type = 'client_credentials'
+		client_id = self.client_id
+		client_secret = self.client_secret
+		grant_type = self.grant_type
 		scope = ['', '', '', '']
 		soid = ''
 		token_url = ''
@@ -41,8 +26,21 @@ class ZohoBooksAPI(Document):
 		oauth = OAuth2Session(client=client)
 
 		#oauth = OAuth2Session(client_id, scope=scope)
-		token = oauth.fetch_token(token_url=token_url, client_id=client_id, client_secret=client_secret,
-							grant_type=grant_type, scope=scope, soid=soid) """
+		#token = oauth.fetch_token(token_url=token_url, client_id=client_id, client_secret=client_secret, grant_type=grant_type, scope=scope, soid=soid)
+
+		""" with requests.Session() as s:
+			s.params = {
+				'client_id': self.client_id,
+				'client_secret': self.client_secret,
+				'grant_type': self.grant_type,
+				'scope': self.scope,
+				'soid': self.soid
+				}
+
+			r = s.get(self.token_url)
+			r.raise_for_status()
+
+			return r.json() """
 
 
 def fetch_unsynced_sales_invoice_list():
