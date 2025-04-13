3d5a337daf2502495c6274f7bfe4ac2634229827
Karan Wilson <karan.wilson@auroville.org.in>
2025-04-14 05:07:42 +0530

    fix: factoring in warehouse name difference between different units/companies - in the unbilled_sales_order_stock report

--- pourtous/pourtous/report/unbilled_sales_order_stock/unbilled_sales_order_stock.py
+++ pourtous/pourtous/report/unbilled_sales_order_stock/unbilled_sales_order_stock.py
@@ -22,70 +22,138 @@ def execute(filters=None):
 
 
 def get_columns():
-	return [
-		{
-			"fieldname": "item_code",
-			"label": "Item Code",
-			"fieldtype": "Data",
-			"width": "100"
-		},
-		{
-			"fieldname": "item_name",
-			"label": "Item Name",
-			"fieldtype": "Data",
-			"width": "200"
-		},
-		{
-			"fieldname": "stock_uom",
-			"label": "UOM",
-			"fieldtype": "Data",
-			"width": "80"
-		},
-		{
-			"fieldname": "order_qty",
-			"label": "Order Qty",
-			"fieldtype": "Float",
-			"width": "100"
-		},
-		{
-			"fieldname": "stall_qty",
-			"label": "Stall Qty",
-			"fieldtype": "Float",
-			"width": "100"
-		},
-		{
-			"fieldname": "qty_needed",
-			"label": "Qty Needed",
-			"fieldtype": "Float",
-			"width": "100"
-		}
-	]
+	if frappe.defaults.get_user_default("company") == 'Pour Tous Purchasing Service':
+		return [
+			{
+				"fieldname": "item_code",
+				"label": "Item Code",
+				"fieldtype": "Data",
+				"width": "100"
+			},
+			{
+				"fieldname": "item_name",
+				"label": "Item Name",
+				"fieldtype": "Data",
+				"width": "200"
+			},
+			{
+				"fieldname": "stock_uom",
+				"label": "UOM",
+				"fieldtype": "Data",
+				"width": "80"
+			},
+			{
+				"fieldname": "order_qty",
+				"label": "Order Qty",
+				"fieldtype": "Float",
+				"width": "100"
+			},
+			{
+				"fieldname": "stall_qty",
+				"label": "Stall Qty",
+				"fieldtype": "Float",
+				"width": "100"
+			},
+			{
+				"fieldname": "qty_needed",
+				"label": "Qty Needed",
+				"fieldtype": "Float",
+				"width": "100"
+			}
+		]
+	
+	else:
+		return [
+			{
+				"fieldname": "item_code",
+				"label": "Item Code",
+				"fieldtype": "Data",
+				"width": "100"
+			},
+			{
+				"fieldname": "item_name",
+				"label": "Item Name",
+				"fieldtype": "Data",
+				"width": "200"
+			},
+			{
+				"fieldname": "stock_uom",
+				"label": "UOM",
+				"fieldtype": "Data",
+				"width": "80"
+			},
+			{
+				"fieldname": "order_qty",
+				"label": "Order Qty",
+				"fieldtype": "Float",
+				"width": "100"
+			},
+			{
+				"fieldname": "stores_qty",
+				"label": "Stores Qty",
+				"fieldtype": "Float",
+				"width": "100"
+			},
+			{
+				"fieldname": "qty_needed",
+				"label": "Qty Needed",
+				"fieldtype": "Float",
+				"width": "100"
+			}
+		]
 
 
 def get_data():
-	query = frappe.db.sql(
-		"""
-		SELECT table1.*,
-		IF ((table1.order_qty > table1.stall_qty), (table1.order_qty - table1.stall_qty), 0) AS qty_needed
+	if frappe.defaults.get_user_default("company") == 'Pour Tous Purchasing Service':
+		query = frappe.db.sql(
+			"""
+			SELECT table1.*,
+			IF ((table1.order_qty > table1.stall_qty), (table1.order_qty - table1.stall_qty), 0) AS qty_needed
 
-		FROM
-		(SELECT i.item_code, i.item_name, i.stock_uom, SUM(i.stock_qty) AS order_qty,
-		(
-			SELECT `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
-			WHERE (`tabStock Ledger Entry`.item_code = i.item_code) AND `tabStock Ledger Entry`.is_cancelled=0
-			AND warehouse like '{0}'
-			ORDER BY posting_date desc, posting_time desc, creation desc
-			LIMIT 1
-		) AS stall_qty
-		FROM `tabSales Order Item` i , `tabSales Order` s
+			FROM
+			(SELECT i.item_code, i.item_name, i.stock_uom, SUM(i.stock_qty) AS order_qty,
+			(
+				SELECT `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+				WHERE (`tabStock Ledger Entry`.item_code = i.item_code) AND `tabStock Ledger Entry`.is_cancelled=0
+				AND warehouse like '{0}'
+				ORDER BY posting_date desc, posting_time desc, creation desc
+				LIMIT 1
+			) AS stall_qty
+			FROM `tabSales Order Item` i , `tabSales Order` s
 
-		where s.status not in ("Closed", "On Hold")
-		AND s.per_billed < 99.99
-		AND s.name = i.parent and i.docstatus = 1
+			where s.status not in ("Closed", "On Hold")
+			AND s.per_billed < 99.99
+			AND s.name = i.parent and i.docstatus = 1
 
-		GROUP BY i.item_code) table1
-		""".format("Stall%"),
-		as_dict=True
-	)
+			GROUP BY i.item_code) table1
+			""".format("Stall%"),
+			as_dict=True
+		)
+
+	else:
+		query = frappe.db.sql(
+			"""
+			SELECT table1.*,
+			IF ((table1.order_qty > table1.stores_qty), (table1.order_qty - table1.stores_qty), 0) AS qty_needed
+
+			FROM
+			(SELECT i.item_code, i.item_name, i.stock_uom, SUM(i.stock_qty) AS order_qty,
+			(
+				SELECT `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+				WHERE (`tabStock Ledger Entry`.item_code = i.item_code) AND `tabStock Ledger Entry`.is_cancelled=0
+				ORDER BY posting_date desc, posting_time desc, creation desc
+				LIMIT 1
+			) AS stores_qty
+			FROM `tabSales Order Item` i , `tabSales Order` s
+
+			where s.status not in ("Closed", "On Hold")
+			AND s.per_billed < 99.99
+			AND s.name = i.parent and i.docstatus = 1
+
+			GROUP BY i.item_code) table1
+			""",
+			as_dict=True
+		)
+
 
 	return query
