e2aa79de663a8972b366c9c508fd48ec90768213
Karan Wilson <karan.wilson@auroville.org.in>
2025-07-02 09:48:06 +0530

    feat: phone and email in print formats Replicate price_list_rate to selling price in Purchase Receipts, in case Selling price is not specified

--- pourtous/hooks.py
+++ pourtous/hooks.py
@@ -150,6 +150,7 @@ doc_events = {
 	},
     # comment the hook below until the pricing rule/method is defined
 	"Purchase Receipt": {
+        "before_save": "auroville.api.update_selling_price", # Add the 'Item Price'
 		"on_submit": "pourtous.api.update_price_lists", # Add the 'Item Price'
         #"before_insert": "auroville.api.get_purchase_tax_template",
 	},
