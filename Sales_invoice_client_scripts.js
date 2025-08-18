b918229ef3b6985fbaefb5baeb436e669bfba2db
Karan Wilson <karan.wilson@auroville.org.in>
2025-08-18 12:23:23 +0530

    CI: Sales Order script modified

--- Sales_invoice_client_scripts.js
+++ Sales_invoice_client_scripts.js
@@ -165,21 +165,25 @@ frappe.listview_settings['Sales Invoice'] = {
                 async: false,
                 callback: (r) => {
                     if (r.message) {
-                        const length = r.message.length;
+                        const length = r.message[0].length;
                         console.log("Number of SO to process: ", length);
 						console.log("Sales Orders to Invoice: ", r.message);
-                        //console.log("is_tax_inclusive: ", r.message[1]);
-                        //console.log("r.message[0][0]: ", r.message[0][0]);
+						console.log("SO to SI: ", r.message[0][0]);
+                        /* console.log("r.message[0][0][0]: ", r.message[0][0][0]);
+                        console.log("r.message[0][0][1]: ", r.message[0][0][1]);
+                        console.log("r.message[0][10][2]: ", r.message[0][10][0]);
+                        console.log("r.message[0][10][3]: ", r.message[0][10][1]);
+                        console.log("is_tax_inclusive: ", r.message[1]); */
                         let done = 0, error = 0;
                         for (let i = 0; i < length; i++) {
                             setTimeout(() => {
                                 frappe.call({
                                     method: 'pourtous.api.process_orders_to_invoice',
                                     args: {
-                                        order: r.message[i][0],
-                                        pos_profile: r.message[i][1],
-                                        order_date: r.message[i][2],
-                                        order_time: r.message[i][3],
+                                        order: r.message[0][i][0],
+                                        pos_profile: r.message[0][i][1],
+                                        order_date: r.message[0][i][2],
+                                        order_time: r.message[0][i][3],
                                         is_tax_inclusive: r.message[1],
                                     },
                                     async: false,
