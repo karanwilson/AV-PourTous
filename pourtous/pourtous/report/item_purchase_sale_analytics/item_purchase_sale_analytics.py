23c2188ecafe08396e60fecf50b9bd793ebf795f
Karan Wilson <karan.wilson@auroville.org.in>
2025-08-29 08:52:59 +0530

    CI: Item Supplier Analytics report modified

--- pourtous/pourtous/report/item_purchase_sale_analytics/item_purchase_sale_analytics.py
+++ pourtous/pourtous/report/item_purchase_sale_analytics/item_purchase_sale_analytics.py
@@ -6,7 +6,8 @@ from frappe import _, msgprint
 
 
 def execute(filters=None):
-	if not (filters.supplier and filters.from_date and filters.to_date):
+	#if not (filters.supplier and filters.from_date and filters.to_date):
+	if not (filters.from_date and filters.to_date):
 		# don't execute until filters are set
 		return [], []
 
@@ -26,9 +27,9 @@ def get_columns():
 	return [
 		{
 			"fieldname": "item_code",
-			"label": "Item Code",
+			"label": "Code",
 			"fieldtype": "Data",
-			"width": "130"
+			"width": "70"
 		},
 
 		{
@@ -36,6 +37,13 @@ def get_columns():
 			"label": "Item Name",
 			"fieldtype": "Data",
 			"width": "300"
+		},
+
+		{
+			"fieldname": "supplier",
+			"label": "Supplier",
+			"fieldtype": "Data",
+			"width": "200"
 		},
 
 		{
@@ -69,31 +77,57 @@ def get_columns():
 
 
 def get_data(filters):
+	if filters.supplier:
+		query = frappe.db.sql(
+			"""
+			select sle.item_code, tabItem.item_name, `tabItem Supplier`.supplier, tabItem.item_group, tabItem.stock_uom,
+			(
+				select SUM(actual_qty) from `tabStock Ledger Entry`
+				where item_code = `tabItem Supplier`.parent
+				AND (`tabStock Ledger Entry`.voucher_type = 'Purchase Invoice' OR `tabStock Ledger Entry`.voucher_type = 'Purchase Receipt')
+				AND `tabStock Ledger Entry`.posting_date BETWEEN '{1}' AND '{2}'
+			) AS qty_purchased,
 
-	query = frappe.db.sql(
-		"""
-		select sle.item_code, tabItem.item_name, tabItem.item_group, tabItem.stock_uom,
-		(
-			select SUM(actual_qty) from `tabStock Ledger Entry`
-			where item_code = `tabItem Supplier`.parent
-			AND (`tabStock Ledger Entry`.voucher_type = 'Purchase Invoice' OR `tabStock Ledger Entry`.voucher_type = 'Purchase Receipt')
-			AND `tabStock Ledger Entry`.posting_date BETWEEN '{1}' AND '{2}'
-		) AS qty_purchased,
+			(
+				select SUM(actual_qty) from `tabStock Ledger Entry`
+				where item_code = `tabItem Supplier`.parent
+				AND voucher_type = "Sales Invoice"
+				AND `tabStock Ledger Entry`.posting_date BETWEEN '{1}' AND '{2}'
+			) AS qty_sold
+			FROM `tabStock Ledger Entry` sle, `tabItem Supplier`, tabItem
+			WHERE sle.is_cancelled = 0
+			AND sle.item_code = `tabItem Supplier`.parent
+			AND sle.item_code = tabItem.item_code
+			AND `tabItem Supplier`.supplier = '{0}'
+			GROUP BY sle.item_code
+			""".format(filters.supplier, filters.from_date, filters.to_date),
+			as_dict=True
+		)
 
-		(
-			select SUM(actual_qty) from `tabStock Ledger Entry`
-			where item_code = `tabItem Supplier`.parent
-			AND voucher_type = "Sales Invoice"
-			AND `tabStock Ledger Entry`.posting_date BETWEEN '{1}' AND '{2}'
-		) AS qty_sold
-		FROM `tabStock Ledger Entry` sle, `tabItem Supplier`, tabItem
-		WHERE sle.is_cancelled = 0
-		AND sle.item_code = `tabItem Supplier`.parent
-		AND sle.item_code = tabItem.item_code
-		AND`tabItem Supplier`.supplier = '{0}'
-		GROUP BY sle.item_code
-		""".format(filters.supplier, filters.from_date, filters.to_date),
-		as_dict=True
-	)
+	else:
+		query = frappe.db.sql(
+			"""
+			select sle.item_code, tabItem.item_name, `tabItem Supplier`.supplier, tabItem.item_group, tabItem.stock_uom,
+			(
+				select SUM(actual_qty) from `tabStock Ledger Entry`
+				where item_code = `tabItem Supplier`.parent
+				AND (`tabStock Ledger Entry`.voucher_type = 'Purchase Invoice' OR `tabStock Ledger Entry`.voucher_type = 'Purchase Receipt')
+				AND `tabStock Ledger Entry`.posting_date BETWEEN '{1}' AND '{2}'
+			) AS qty_purchased,
+
+			(
+				select SUM(actual_qty) from `tabStock Ledger Entry`
+				where item_code = `tabItem Supplier`.parent
+				AND voucher_type = "Sales Invoice"
+				AND `tabStock Ledger Entry`.posting_date BETWEEN '{1}' AND '{2}'
+			) AS qty_sold
+			FROM `tabStock Ledger Entry` sle, `tabItem Supplier`, tabItem
+			WHERE sle.is_cancelled = 0
+			AND sle.item_code = `tabItem Supplier`.parent
+			AND sle.item_code = tabItem.item_code
+			GROUP BY sle.item_code
+			""".format(filters.supplier, filters.from_date, filters.to_date),
+			as_dict=True
+		)
 
 	return query
