9d3d9e16aead05701da4133f3788355b0713a75e
Karan Wilson <karan.wilson@auroville.org.in>
2025-12-30 19:41:42 +0530

    feat: monthly_statement report

--- pourtous/pourtous/report/monthly_statement/monthly_statement.py
+++ pourtous/pourtous/report/monthly_statement/monthly_statement.py
@@ -91,57 +91,49 @@ def get_columns(filters):
 				"options": "Sales Invoice",
 				"width": "150"
 			},
-
 			{
 				"fieldname": "customer_name",
 				"label": "Customer",
 				"fieldtype": "Data",
 				"width": "130"
 			},
-
 			{
 				"fieldname": "custom_fs_account_number",
 				"label": "FS Account",
 				"fieldtype": "Data",
 				"width": "130"
 			},
-
 			{
 				"fieldname": "posting_date",
 				"label": "Posting Date",
 				"fieldtype": "Date",
 				"width": "130"
 			},
-
 			{
 				"fieldname": "total",
 				"label": "Net Total",
 				"fieldtype": "Currency",
 				"width": "100"
 			},
-
 			{
 				"fieldname": "total_taxes_and_charges",
 				"label": "Total Tax",
 				"fieldtype": "Currency",
 				"width": "100"
 			},
-
 			{
 				"fieldname": "grand_total",
 				"label": "Grand Total",
 				"fieldtype": "Currency",
 				"width": "100"
 			},
-
 			{
 				"fieldname": "return_against",
 				"label": "Return Against",
 				"fieldtype": "Link",
 				"options": "Sales Invoice",
 				"width": "130"
-			},
-
+			}
 		]
 
 
@@ -149,11 +141,13 @@ def get_data(filters):
 	if filters.items_view:
 		query = frappe.db.sql(
 			"""
-			SELECT s.name AS invoice_name, s.customer_name, s.custom_fs_account_number, s.posting_date, si.item_code, si.item_name, si.qty, si.rate, si.amount
-			FROM `tabSales Invoice Item` si, `tabSales Invoice` s
-			WHERE s.docstatus = 1 AND s.custom_fs_account_number = '{0}'
-			AND s.posting_date BETWEEN '{1}' AND '{2}'
-			AND si.parent = s.name
+			SELECT si.name AS invoice_name, si.customer_name, si.custom_fs_account_number, si.posting_date,
+			sii.item_code, sii.item_name, sii.qty, sii.rate, sii.amount
+			FROM `tabSales Invoice Item` sii, `tabSales Invoice` si
+			WHERE si.docstatus = 1
+			AND sii.parent = si.name
+			AND si.custom_fs_account_number = '{0}'
+			AND si.posting_date BETWEEN '{1}' AND '{2}'
 			""".format(filters.custom_fs_account_number, filters.from_date, filters.to_date),
 			as_dict=True
 		)
@@ -161,12 +155,17 @@ def get_data(filters):
 	else:
 		query = frappe.db.sql(
 			"""
-			SELECT name, customer_name, custom_fs_account_number, posting_date, total, total_taxes_and_charges,
-			grand_total, return_against
-
-			FROM `tabSales Invoice` WHERE docstatus = 1
-			AND posting_date between '{0}' and '{1}'
-			AND custom_fs_account_number = '{2}'
+			SELECT si.name, si.customer_name, si.custom_fs_account_number, si.posting_date, si.total, si.total_taxes_and_charges,
+			si.grand_total, si.return_against
+			FROM `tabSales Invoice` si, `tabSales Invoice Item` sii
+			LEFT JOIN `tabSales Order` so 
+			ON so.docstatus = 1
+			AND so.name = sii.sales_order
+			WHERE si.docstatus = 1
+			AND sii.parent = si.name
+			AND si.posting_date between '{0}' and '{1}'
+			AND si.custom_fs_account_number = '{2}'
+			GROUP BY si.name
 			""".format(filters.from_date, filters.to_date, filters.custom_fs_account_number),
 			as_dict=True
 		)
