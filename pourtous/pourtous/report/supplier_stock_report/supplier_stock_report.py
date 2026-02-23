b13ea20b2d2e3ef857e434f7f5e206a03ba85da7
Karan Wilson <karan.wilson@auroville.org.in>
2026-02-23 08:16:32 +0530

    CI: added new Warehouses to Supplier stock Report

--- pourtous/pourtous/report/supplier_stock_report/supplier_stock_report.py
+++ pourtous/pourtous/report/supplier_stock_report/supplier_stock_report.py
@@ -21,124 +21,268 @@ def execute(filters=None):
 
 
 def get_columns():
-	return [
-		{
-			"fieldname": "item_code",
-			"label": "Item Code",
-			"fieldtype": "Data",
-			"width": "90"
-		},
-		{
-			"fieldname": "item_name",
-			"label": "Item Name",
-			"fieldtype": "Data",
-			"width": "250"
-		},
-		{
-			"fieldname": "item_group",
-			"label": "Item Group",
-			"fieldtype": "Data",
-			"width": "200"
-		},
-		{
-			"fieldname": "supplier",
-			"label": "Supplier",
-			"fieldtype": "Data",
-			"width": "200"
-		},
-		{
-			"fieldname": "store_qty",
-			"label": "Store Qty",
-			"fieldtype": "Float",
-			"width": "100"
-		},
-		{
-			"fieldname": "stall_qty",
-			"label": "Stall Qty",
-			"fieldtype": "Float",
-			"width": "100"
-		},
-		{
-			"fieldname": "so_reserve",
-			"label": "SO Reserve",
-			"fieldtype": "Float",
-			"width": "115"
-		},
-	]
+	if frappe.defaults.get_user_default("company") == "Pour Tous Purchasing Service":
+		return [
+			{
+				"fieldname": "item_code",
+				"label": "Item Code",
+				"fieldtype": "Data",
+				"width": "90"
+			},
+			{
+				"fieldname": "item_name",
+				"label": "Item Name",
+				"fieldtype": "Data",
+				"width": "250"
+			},
+			{
+				"fieldname": "item_group",
+				"label": "Item Group",
+				"fieldtype": "Data",
+				"width": "200"
+			},
+			{
+				"fieldname": "supplier",
+				"label": "Supplier",
+				"fieldtype": "Data",
+				"width": "200"
+			},
+			{
+				"fieldname": "stores_qty",
+				"label": "Stores",
+				"fieldtype": "Float",
+				"width": "90"
+			},
+			{
+				"fieldname": "stall_qty",
+				"label": "Stall",
+				"fieldtype": "Float",
+				"width": "80"
+			},
+			{
+				"fieldname": "store_1_qty",
+				"label": "Store1",
+				"fieldtype": "Float",
+				"width": "80"
+			},
+			{
+				"fieldname": "store_2_qty",
+				"label": "Store2",
+				"fieldtype": "Float",
+				"width": "80"
+			},
+			{
+				"fieldname": "store_3_qty",
+				"label": "Store3",
+				"fieldtype": "Float",
+				"width": "80"
+			},
+		]
+	
+	else:
+		return [
+			{
+				"fieldname": "item_code",
+				"label": "Item Code",
+				"fieldtype": "Data",
+				"width": "90"
+			},
+			{
+				"fieldname": "item_name",
+				"label": "Item Name",
+				"fieldtype": "Data",
+				"width": "250"
+			},
+			{
+				"fieldname": "item_group",
+				"label": "Item Group",
+				"fieldtype": "Data",
+				"width": "200"
+			},
+			{
+				"fieldname": "supplier",
+				"label": "Supplier",
+				"fieldtype": "Data",
+				"width": "200"
+			},
+			{
+				"fieldname": "store_qty",
+				"label": "Store Qty",
+				"fieldtype": "Float",
+				"width": "100"
+			},
+			{
+				"fieldname": "stall_qty",
+				"label": "Stall Qty",
+				"fieldtype": "Float",
+				"width": "100"
+			},
+		]
 
 
 def get_data(filters):
-	if filters.supplier:
-		query = frappe.db.sql(
-			"""
-			SELECT table1.*
-			FROM
+	if frappe.defaults.get_user_default("company") == "Pour Tous Purchasing Service":
+		if filters.supplier:
+			query = frappe.db.sql(
+				"""
+				SELECT table1.*
+				FROM
 
-			(
-			SELECT tabItem.item_code, tabItem.item_name, tabItem.item_group, `tabItem Supplier`.supplier,
-			(
-				select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
-				where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
-				and warehouse like '{0}'
-				order by posting_date desc, posting_time desc, creation desc
-				limit 1
-			) AS store_qty,
-			(
-				select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
-				where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
-				and warehouse like '{1}'
-				order by posting_date desc, posting_time desc, creation desc
-				limit 1
-			) AS stall_qty,
-			(
-				select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
-				where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
-				and warehouse like '{2}'
-				order by posting_date desc, posting_time desc, creation desc
-				limit 1
-			) AS so_reserve
-			FROM tabItem, `tabItem Supplier`
-			WHERE tabItem.item_code = `tabItem Supplier`.parent
-			AND `tabItem Supplier`.supplier = '{3}'
-			) table1
-			""".format("Stores%", "Stall%", "Sales Order Reserve%", filters.supplier),
-			as_dict=True
-		)
+				(
+				SELECT tabItem.item_code, tabItem.item_name, tabItem.item_group, `tabItem Supplier`.supplier,
+				(
+					select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+					where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+					and warehouse like '{0}'
+					order by posting_date desc, posting_time desc, creation desc
+					limit 1
+				) AS store_qty,
+				(
+					select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+					where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+					and warehouse like '{1}'
+					order by posting_date desc, posting_time desc, creation desc
+					limit 1
+				) AS stall_qty,
+				(
+					select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+					where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+					and warehouse like '{2}'
+					order by posting_date desc, posting_time desc, creation desc
+					limit 1
+				) AS store_1_qty,
+				(
+					select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+					where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+					and warehouse like '{3}'
+					order by posting_date desc, posting_time desc, creation desc
+					limit 1
+				) AS store_2_qty,
+				(
+					select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+					where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+					and warehouse like '{4}'
+					order by posting_date desc, posting_time desc, creation desc
+					limit 1
+				) AS store_3_qty
+				FROM tabItem, `tabItem Supplier`
+				WHERE tabItem.item_code = `tabItem Supplier`.parent
+				AND `tabItem Supplier`.supplier = '{5}'
+				) table1
+				""".format("Stores%", "Stall%", "Store 1%", "Store 2%", "Store 3%", filters.supplier),
+				as_dict=True
+			)
 
+		else:
+			query = frappe.db.sql(
+				"""
+				SELECT table1.*
+				FROM
+
+				(
+				SELECT tabItem.item_code, tabItem.item_name, tabItem.item_group, `tabItem Supplier`.supplier,
+				(
+					select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+					where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+					and warehouse like '{0}'
+					order by posting_date desc, posting_time desc, creation desc
+					limit 1
+				) AS store_qty,
+				(
+					select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+					where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+					and warehouse like '{1}'
+					order by posting_date desc, posting_time desc, creation desc
+					limit 1
+				) AS stall_qty,
+				(
+					select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+					where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+					and warehouse like '{2}'
+					order by posting_date desc, posting_time desc, creation desc
+					limit 1
+				) AS store_1_qty,
+				(
+					select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+					where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+					and warehouse like '{3}'
+					order by posting_date desc, posting_time desc, creation desc
+					limit 1
+				) AS store_2_qty,
+				(
+					select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+					where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+					and warehouse like '{4}'
+					order by posting_date desc, posting_time desc, creation desc
+					limit 1
+				) AS store_3_qty
+				FROM tabItem, `tabItem Supplier`
+				WHERE tabItem.item_code = `tabItem Supplier`.parent
+				) table1
+				""".format("Stores%", "Stall%", "Store 1%", "Store 2%", "Store 3%"),
+				as_dict=True
+			)
+
+		return query
+	
 	else:
-		query = frappe.db.sql(
-			"""
-			SELECT table1.*
-			FROM
+		if filters.supplier:
+			query = frappe.db.sql(
+				"""
+				SELECT table1.*
+				FROM
 
-			(
-			SELECT tabItem.item_code, tabItem.item_name, tabItem.item_group, `tabItem Supplier`.supplier,
-			(
-				select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
-				where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
-				and warehouse like '{0}'
-				order by posting_date desc, posting_time desc, creation desc
-				limit 1
-			) AS store_qty,
-			(
-				select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
-				where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
-				and warehouse like '{1}'
-				order by posting_date desc, posting_time desc, creation desc
-				limit 1
-			) AS stall_qty,
-			(
-				select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
-				where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
-				and warehouse like '{2}'
-				order by posting_date desc, posting_time desc, creation desc
-				limit 1
-			) AS so_reserve
-			FROM tabItem, `tabItem Supplier`
-			WHERE tabItem.item_code = `tabItem Supplier`.parent
-			) table1
-			""".format("Stores%", "Stall%", "Sales Order Reserve%"),
-			as_dict=True
-		)
+				(
+				SELECT tabItem.item_code, tabItem.item_name, tabItem.item_group, `tabItem Supplier`.supplier,
+				(
+					select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+					where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+					and warehouse like '{0}'
+					order by posting_date desc, posting_time desc, creation desc
+					limit 1
+				) AS store_qty,
+				(
+					select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+					where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+					and warehouse like '{1}'
+					order by posting_date desc, posting_time desc, creation desc
+					limit 1
+				) AS stall_qty,
+				FROM tabItem, `tabItem Supplier`
+				WHERE tabItem.item_code = `tabItem Supplier`.parent
+				AND `tabItem Supplier`.supplier = '{3}'
+				) table1
+				""".format("Stores%", "Stall%", filters.supplier),
+				as_dict=True
+			)
 
-	return query
+		else:
+			query = frappe.db.sql(
+				"""
+				SELECT table1.*
+				FROM
+
+				(
+				SELECT tabItem.item_code, tabItem.item_name, tabItem.item_group, `tabItem Supplier`.supplier,
+				(
+					select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+					where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+					and warehouse like '{0}'
+					order by posting_date desc, posting_time desc, creation desc
+					limit 1
+				) AS store_qty,
+				(
+					select `tabStock Ledger Entry`.qty_after_transaction from `tabStock Ledger Entry`
+					where (`tabStock Ledger Entry`.item_code = tabItem.item_code) and `tabStock Ledger Entry`.is_cancelled=0
+					and warehouse like '{1}'
+					order by posting_date desc, posting_time desc, creation desc
+					limit 1
+				) AS stall_qty,
+				FROM tabItem, `tabItem Supplier`
+				WHERE tabItem.item_code = `tabItem Supplier`.parent
+				) table1
+				""".format("Stores%", "Stall%"),
+				as_dict=True
+			)
+
+		return query
