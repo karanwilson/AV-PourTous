461df7d3c4e1ddc3fa10b9b629d602bd302d8efe
Karan Wilson <karan.wilson@auroville.org.in>
2026-03-15 21:16:14 +0530

    CI: interim commit: set supplier bill numbers as unique in purchase invoice

--- sync_bills_debitnotes_zoho.js
+++ sync_bills_debitnotes_zoho.js
@@ -119,6 +119,44 @@ frappe.listview_settings['Purchase Invoice'] = {
             });
         }, __("Sync with ZB"));
 
+        listview.page.add_inner_button("Set Vendor Bills unique", () => {
+            frappe.call({
+                method: 'pourtous.api.fetch_purchase_invoices',
+                async: false,
+                callback: (r) => {
+                    if (r.message) {
+                        const length = r.message.length;
+                        console.log("Number of Bills to add: ", length);
+						console.log("Bills List: ", r.message);
+                        console.log("r.message[0]['name']: ", r.message[0]["name"]);
+                        /* let added = 0;
+                        for (let i = 0; i < length; i++) {
+                            setTimeout(() => {
+                                frappe.call({
+                                    method: 'pourtous.api.set_vendor_bills_unique',
+                                    args: {
+                                        bill: r.message[i]["name"]
+                                    },
+                                    async: false,
+                                }).then(r => {
+                                    if (r.message == "ADDED")
+                                        added++;
+                                }).then(r => {
+                                    // placing this statement block here as it does not work outside of the main frappe.call block
+                                    // though it prints on console for each loop iteration (comes in only one line, with the loop count),
+                                    // it shows an accurate result in the end. This design works.
+                                    console.log("Added ", added, ", of ", length);
+                                });
+                                const count = i+1;
+                                const message = "Adding "+count+" of "+length;
+                                frappe.show_progress("Pushing Bills to Zoho Books", count, length, message);
+                            }, 0);
+                        } */
+                    }
+                }
+            });
+        });
+
         /* listview.page.add_inner_button("Delete specific bills in ZB", () => {
             frappe.call({
                 method: 'pourtous.pourtous.doctype.zoho_books_api.zoho_books_api.fetch_bills_to_delete',
